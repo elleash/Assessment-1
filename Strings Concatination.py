42bc48ca65b75ebf78a3d15a97960f97937cf709
elleash <105912749+elleash@users.noreply.github.com>
2022-12-09 14:27:51 +0800

    Delete Strings Concatination.py

--- Strings Concatination.py
+++
@@ -1,6 +0,0 @@
-#asks the user to input their subjects
-x = input("Enter your first subject:")
-y = input("Enter your second subject:")
-z = input("Enter your third subject:")
-
-print("Your schedule is:", x+" "+y+" "+z) #using the + operator, the subject would be printed together

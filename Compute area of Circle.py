e8f952170086b21baeae7902deb34d97dde759f1
elleash <105912749+elleash@users.noreply.github.com>
2022-12-09 14:27:08 +0800

    Delete Compute area of Circle.py

--- Compute area of Circle.py
+++
@@ -1,6 +0,0 @@
-from math import pi #import the pi module
-
-rad = int(input("Enter radius of the circle:")) #asks the user to enter any number they want
-area = pi*rad**2 #formula for finding out the area of a circle
-
-print("The area of the circle is:", area) #the output of the area of pi

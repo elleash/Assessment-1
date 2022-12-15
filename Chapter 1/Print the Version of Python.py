5a1b1d9e7253ed7d0a428b62958c8f8f68026412
elleash <105912749+elleash@users.noreply.github.com>
2022-12-15 15:17:03 +0800

    Update Print the Version of Python.py

--- Chapter 1/Print the Version of Python.py
+++ Chapter 1/Print the Version of Python.py
@@ -1,5 +1,5 @@
 import sys #import sys module
-print("The Python Version of this computer:")
+print("The Python Version of this computer:") #a message to print the version of python
 print(sys.version) #prints the version of python
-print("Python Version info.")
+print("Python Version info.") #a statement to print the information about the vrsion of python
 print(sys.version_info) #print the information about python

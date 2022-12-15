25e5283b792b1a0f4012944ebcf4bdc90099b04f
elleash <105912749+elleash@users.noreply.github.com>
2022-12-15 19:09:48 +0800

    Update Greetings.py

--- Chapter 3/Greetings.py
+++ Chapter 3/Greetings.py
@@ -1,7 +1,6 @@
-names = ["cleo", "max", "sam", "manly" ]
+names = ["cleo", "max", "sam", "manly" ] #list of names
 
-#accessing each elements in the list with a message
-print("Good day", names[0].title(),"! You are doing great")
-print("Good day", names[1].title(),"! You are doing great")
-print("Good day", names[2].title(),"! You are doing great")
-print("Good day", names[3].title(),"! You are doing great")
+print("Good day", names[0].title(),"! You are doing great") #accessing the first name in the list with a message
+print("Good day", names[1].title(),"! You are doing great") #accessing the second name in the list with a message
+print("Good day", names[2].title(),"! You are doing great") #accessing the third name in the list with a message
+print("Good day", names[3].title(),"! You are doing great") #accessing the fourth name in the list with a message

ef73c933d731f27f347a91c39c25431cd5932826
elleash <105912749+elleash@users.noreply.github.com>
2022-12-15 18:52:07 +0800

    Update Seeing the world.py

--- Chapter 3/Seeing the world.py
+++ Chapter 3/Seeing the world.py
@@ -1,31 +1,31 @@
-place = ["Norway", "Italy", "France", "Mexico"]
-print(place)
+place = ["Norway", "Italy", "France", "Mexico"] #list of places
+print(place) #prints the list
 
-print("\nThe original list is:")
-print(place)
+print("\nThe original list is:") #displays message about the list information
+print(place) #displays the list
 
-print("\nThis is the list in alphabetical order:")
+print("\nThis is the list in alphabetical order:") #displays message about the list information
 print(sorted(place)) #arranges the list in alphabetical order
-print("\nThe original order:")
-print(place)
+print("\nThe original order:") #displays message about the list information
+print(place) #displays the list
 
-print("\nThis is the list in reverse order:")
+print("\nThis is the list in reverse order:") #displays message about the list information
 print(sorted(place, reverse = True)) #arranges the list in reverse alphabetical order
-print("\nThe original order:")
-print(place)
+print("\nThe original order:") #displays message about the list information
+print(place) #displays the list
 
-print("\nThis is the list in reverse order:")
+print("\nThis is the list in reverse order:") #displays message about the list information
 place.reverse() #reverse the list in order
-print(place)
+print(place) #displays the list
 
-print("\nThe original order:")
+print("\nThe original order:") #displays message about the list information
 place.reverse() #reverses it back to the original order
-print(place)
+print(place) #displays the list
 
-print("\nThis is the list in alphabetical order:")
+print("\nThis is the list in alphabetical order:") #displays message about the list information
 place.sort() #arranges the list in alphabetical order
-print(place)
+print(place) #displays the list
 
-print("\nThis is the list in reversed alphabetical oder:")
+print("\nThis is the list in reversed alphabetical oder:") #displays message about the list information
 place.sort(reverse = True) #arranges list in reverse alphabetical
-print(place)
+print(place) #displays the list

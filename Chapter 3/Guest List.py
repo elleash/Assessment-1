931d2b236e4c4b2ca70cfffe4e402b5fe653b00e
elleash <105912749+elleash@users.noreply.github.com>
2022-12-15 19:07:17 +0800

    Update Guest List.py

--- Chapter 3/Guest List.py
+++ Chapter 3/Guest List.py
@@ -1,3 +1,3 @@
-guest = ["anne sexton", "silvia plath", "virginia woolf"]
+guest = ["anne sexton", "silvia plath", "virginia woolf"] #list of guests
 for x in guest: #prints the guest individually
-    print(f"{x.title()}, you are cordially invited to attend a dinner at 6 pm tomorrow.")
+    print(f"{x.title()}, you are cordially invited to attend a dinner at 6 pm tomorrow.") #displays the guests being invited

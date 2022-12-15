549671c67cc0f76d3690c83ee385506d45c7e53d
elleash <105912749+elleash@users.noreply.github.com>
2022-12-16 02:50:27 +0800

    Update Rivers.py

--- Chapter 5/Rivers.py
+++ Chapter 5/Rivers.py
@@ -1,14 +1,14 @@
 river = {"nile":"egypt", "amazon":"colombia", "mississippi": "illinois"} #dictionary of rivers
 
-for key, value in river.items(): #prints the dictionary
-   print("The",key.title(), "River runs through",value.title())
+for key, value in river.items(): #to iterate the dictionary
+   print("The",key.title(), "River runs through",value.title()) #displays message with the key and value items
 
-print("\n")
+print("\n") #to add whitespace
 
-for key in river.keys(): #prints the river only
-   print(key.title(), "is a major river of the world!")
+for key in river.keys(): #to iterate the dictionary
+   print(key.title(), "is a major river of the world!") #displays the river only with the message
 
-print("\n")
+print("\n") #to add whitespace
 
-for value in river.values(): #prints the coutry only
-    print(value.title(), "is the country the river passes through!")
+for value in river.values(): #to iterate the dictionary
+    print(value.title(), "is the country the river passes through!") #displays the country only with the message

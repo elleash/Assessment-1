db969fb18187f3bd3890b69bb3b6ac1858af0cad
elleash <105912749+elleash@users.noreply.github.com>
2022-12-16 00:09:16 +0800

    Update Alien Colors 3.py

--- Chapter 4/Alien Colors 3.py
+++ Chapter 4/Alien Colors 3.py
@@ -1,26 +1,26 @@
 alien_color = "green" #assigned color to print the appropriate message
 
-if alien_color == "green":
-    print("You have earned 5 points! Good job!") #if the color is green the player earns 5 points
-elif alien_color == "yellow":
-    print("You have earned 10 points! Amazing!") #if the color is yellow the player earns 10 points
-else:
-    print("You have earned 15 points! Splendid!") #if the color is red, player earns 15 points
+if alien_color == "green": #if the color is green the player earns 5 points
+    print("You have earned 5 points! Good job!") #displays message if color is green
+elif alien_color == "yellow": #if the color is yellow the player earns 10 points
+    print("You have earned 10 points! Amazing!") #displays message if color is yellow
+else: #if the color is red, player earns 15 points
+    print("You have earned 15 points! Splendid!") #display message if color is red
 
 alien_color = "yellow" #assigned color to print the appropriate message
 
-if alien_color == "green":
-    print("You have earned 5 points! Good job!")
-elif alien_color == "yellow":
-    print("You have earned 10 points! Amazing!")
-else:
-    print("You have earned 15 points! Splendid!")
+if alien_color == "green":  #if the color is green the player earns 5 points
+    print("You have earned 5 points! Good job!") #displays message if color is green
+elif alien_color == "yellow":  #if the color is yellow the player earns 10 points
+    print("You have earned 10 points! Amazing!") #displays message if color is yellow
+else:  #if the color is red the player earns 15 points
+    print("You have earned 15 points! Splendid!") #display message if color is red
 
 alien_color = "red" #assigned color to print the appropriate message
 
-if alien_color == "green":
-    print("You have earned 5 points! Good job!")
-elif alien_color == "yellow":
-    print("You have earned 10 points! Amazing!")
-else:
-    print("You have earned 15 points! Splendid!") #if the color is not green, yellow, or red the player earns 15 points
+if alien_color == "green":  #if the color is green the player earns 5 points
+    print("You have earned 5 points! Good job!") #displays message if color is green
+elif alien_color == "yellow":  #if the color is yellow the player earns 10 points
+    print("You have earned 10 points! Amazing!") #displays message if color is yellow
+else:   #if the color is red the player earns 15 points
+    print("You have earned 15 points! Splendid!") #message to be displayed if player got a red color

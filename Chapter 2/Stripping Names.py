e7b6fe29882e7409c0e7aae239100e5226ca6a88
elleash <105912749+elleash@users.noreply.github.com>
2022-12-15 15:46:57 +0800

    Update Stripping Names.py

--- Chapter 2/Stripping Names.py
+++ Chapter 2/Stripping Names.py
@@ -1,11 +1,11 @@
-name = "\n\t  James"
-print("My name is", name) #print the string as it is
+name = "\n\t  James" #the name with whitespaces
+print("My name is", name) #print the string with whitespaces
 
-name = "\n\t  James"
+name = "\n\t  James" #the name with whitespaces
 print("My name is", name.strip()) #using .strip function to strip the whole name
 
-name = "\n\t  James"
+name = "\n\t  James" #the name with whitespaces on the left
 print("My name is", name.lstrip()) #using .lstrip to strip the left side of the string
 
-name = "James\n\t"
+name = "James\n\t" #the name with whitespaces on the right
 print("My name is", name.rstrip()) #using .rstrip to strip the right side of the string

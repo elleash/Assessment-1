37a26b6842995ab49d668caa07967fb284791d48
elleash <105912749+elleash@users.noreply.github.com>
2022-12-15 19:06:32 +0800

    Update Names.py

--- Chapter 3/Names.py
+++ Chapter 3/Names.py
@@ -1,7 +1,6 @@
-names = ["cleo", "max", "sam", "manly" ]
+names = ["cleo", "max", "sam", "manly" ] #list of names
 
-#accessing each name in the list
-print(names[0])
-print(names[1])
-print(names[2])
-print(names[3])
+print(names[0]) #acessing the first name of the list
+print(names[1]) #acessing the second name of the list
+print(names[2]) #acessing the third name of the list
+print(names[3]) #acessing the fourth name of the list

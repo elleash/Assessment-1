87ec450111259926d14dc8d7aa21663016289097
elleash <105912749+elleash@users.noreply.github.com>
2022-12-15 20:04:47 +0800

    Update Favorite Fruit.py

--- Chapter 4/Favorite Fruit.py
+++ Chapter 4/Favorite Fruit.py
@@ -1,13 +1,13 @@
-favorite_fruits = ["banana", "apple", "strawberry"]
+favorite_fruits = ["banana", "apple", "strawberry"] #list of fruits
 
-#if statements to print a message if the entered fruit is in the created list
-if "blueberry" in favorite_fruits:
-    print("This is not your favorite fruit.")
-if "apple" in favorite_fruits:
-    print("Apples are your favorite fruit!")
-if "banana" in favorite_fruits:
-    print("Bananas are your favorite fruit!")
-if "watermelon" in favorite_fruits:
-    print("Watermelons are not your favorite fruits.")
-if "coconut" in favorite_fruits:
-    print("Coconuts are not your favotite fruit.")
+
+if "blueberry" in favorite_fruits:  #check if the given fruit is in the list
+    print("This is not your favorite fruit.") #displays that the given fruit is not in the list
+if "apple" in favorite_fruits: #check if the given fruit is in the list
+    print("Apples are your favorite fruit!") #displays that the given fruit is in the list
+if "banana" in favorite_fruits: #check if the given fruit is in the list
+    print("Bananas are your favorite fruit!") #displays that the given fruit is in the list
+if "watermelon" in favorite_fruits: #check if the given fruit is in the list
+    print("Watermelons are not your favorite fruits.") #displays that the given fruit is not in the list
+if "coconut" in favorite_fruits: #check if the given fruit is in the list
+    print("Coconuts are not your favotite fruit.") #displays that the given fruit is not in the list

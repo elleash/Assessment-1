9f67ffef61c47d78d69cda2385096167f98a0139
elleash <105912749+elleash@users.noreply.github.com>
2022-12-16 01:11:21 +0800

    Update Alien Colors 2.py

--- Chapter 4/Alien Colors 2.py
+++ Chapter 4/Alien Colors 2.py
@@ -1,15 +1,14 @@
-alien_color = "green"
+alien_color = "green" #given variable
 
-#if the alien's color is green then the statement will say player earned five points
-if alien_color == "green":
-    print("You just earned 5 points!")
-#else the player will earn 10 points
-else:
-    print("You just earned 10 points!")
+if alien_color == "green": #if the alien's color is green then user earns 5 points
+    print("You just earned 5 points!") #displays when the color is green
+
+else: #if the color is anything but green, the user earns 10 points
+    print("You just earned 10 points!") #displays when the color is not green
 
 alien_color = "red" #a variable to run the else statement
 
-if alien_color == "green":
-    print("You just earned 5 points!")
-else:
-    print("You just earned 10 points!")
+if alien_color == "green": #if the alien's color is green then user earns 5 points
+    print("You just earned 5 points!") #displays when the color is green
+else: #if the color is anything but green, the user earns 10 points
+    print("You just earned 10 points!") #displays when the color is not green

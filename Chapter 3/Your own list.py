d933eb35fa4257fde0e9de103ffb40d8bb344f5e
elleash <105912749+elleash@users.noreply.github.com>
2022-12-15 16:00:14 +0800

    Update Your own list.py

--- Chapter 3/Your own list.py
+++ Chapter 3/Your own list.py
@@ -1,6 +1,5 @@
-transpo = ["car", "bus", "motorcycle"]
+transpo = ["car", "bus", "motorcycle"] #list of the transportations
 
-#printing a statement for each transporation in the list
-print(f"When the day is hot, I love travelling by {transpo[0]}.")
-print(f"I like travelling by {transpo[1]} when leaving school.")
-print(f"Someday, I would like to own a {transpo[2]} to travel quickly.")
+print(f"When the day is hot, I love travelling by {transpo[0]}.") #prints the frist item with a specified message
+print(f"I like travelling by {transpo[1]} when leaving school.") #prints the second item with a specified message
+print(f"Someday, I would like to own a {transpo[2]} to travel quickly.") #prints the third item with a specified message

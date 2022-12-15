516161012a634f471903271f6e4f64259cbcaab7
elleash <105912749+elleash@users.noreply.github.com>
2022-12-15 23:45:55 +0800

    Update Alien Colors.py

--- Chapter 4/Alien Colors.py
+++ Chapter 4/Alien Colors.py
@@ -1,9 +1,9 @@
 alien_color = "green" #version that passes
 
 if alien_color == "green": #if the variable is green then the player earns 5 points
-    print("You just earned 5 points!")
+    print("You just earned 5 points!") #message displayed if the variable is green
 
 alien_color = "yellow" #version that fails
 
-if alien_color == "green":
-    print("You just earned 5 points!")
+if alien_color == "green": #if statement to test the failed code
+    print("You just earned 5 points!") #message displayed if the variable is green

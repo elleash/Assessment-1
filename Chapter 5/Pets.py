94a469c4e4f5f357d0e8228f3f5cd84121aabe12
elleash <105912749+elleash@users.noreply.github.com>
2022-12-16 03:17:10 +0800

    Update Pets.py

--- Chapter 5/Pets.py
+++ Chapter 5/Pets.py
@@ -1,14 +1,13 @@
-#dictionary of pets
-pet1 = {"name":"gambol", "type": "cat", "color":"black", "owner": "Blake" }
-pet2 = {"name":"ember", "type": "dog", "color":"yellow", "owner": "Yang" }
-pet3 = {"name":"swei", "type": "dog", "color":"black and white", "owner": "Rose" }
-pet4 = {"name":"tena", "type": "cat", "color":"white", "owner": "Weis" }
+pet1 = {"name":"gambol", "type": "cat", "color":"black", "owner": "Blake" } #dictionary of first pet
+pet2 = {"name":"ember", "type": "dog", "color":"yellow", "owner": "Yang" } #dictionary of first pet
+pet3 = {"name":"swei", "type": "dog", "color":"black and white", "owner": "Rose" } #dictionary of first pet
+pet4 = {"name":"tena", "type": "cat", "color":"white", "owner": "Weis" } #dictionary of first pet
 
 pets = [pet1, pet2, pet3, pet4] #the dictionary in a list
 
-print("Information about my friend's pets:\n")
+print("Information about my friend's pets:\n") #displays a message to show info about the pets
 
 for pet in pets: #loops through the list of information about the pets
-    print("\n")
-    for key, value in pet.items(): 
-        print(key.title(), ":", value.title())
+    print("\n") #adds whitespace
+    for key, value in pet.items(): #loops through the dictionary
+        print(key.title(), ":", value.title()) #displays the dictionary

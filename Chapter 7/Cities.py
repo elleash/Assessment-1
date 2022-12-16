e955c1dffa311254d5354f09707bcbabfaf265d0
elleash <105912749+elleash@users.noreply.github.com>
2022-12-16 15:06:06 +0800

    Update Cities.py

--- Chapter 7/Cities.py
+++ Chapter 7/Cities.py
@@ -2,5 +2,5 @@ def describe_city(city,country = "Philippines"): #city function with default cou
     print(f'The {city.title()} city is in {country.title()}') #message to be printed
 
 describe_city(city = "manila") #calls the city parameter
-describe_city(city = 'makati')
+describe_city(city = 'makati') #calls the city parameter
 describe_city(city = 'new york', country = 'united states of america') #overwrites the default countr parameter

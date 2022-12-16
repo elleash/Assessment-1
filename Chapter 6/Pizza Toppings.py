73ad8c6fb2935e51a4063a47fea6d9fcf76db0d9
elleash <105912749+elleash@users.noreply.github.com>
2022-12-16 14:43:17 +0800

    Update Pizza Toppings.py

--- Chapter 6/Pizza Toppings.py
+++ Chapter 6/Pizza Toppings.py
@@ -2,6 +2,7 @@ while True: #loop to ask user continuosly
     pizza = input("Enter the toppings you want on your pizza!\nIf you are done, type quit\n") #asks user to enter toppings
 
     if pizza == "quit": #if they enter "quit" the loop will stop
-                break
-    else:
+                break #stops the loop
+    
+    else: #if user doesn't enter "quit", the loop goes on
         print(f"{pizza} will be added to your pizza!") #users pizza will be added

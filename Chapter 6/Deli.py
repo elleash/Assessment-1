ec3023d9ce43b9375cae235d7da867773ba6d2ba
elleash <105912749+elleash@users.noreply.github.com>
2022-12-16 15:04:19 +0800

    Update Deli.py

--- Chapter 6/Deli.py
+++ Chapter 6/Deli.py
@@ -1,12 +1,12 @@
-sandwich_orders = ["tuna", "egg", "chicken", "vegan"]
-finished_sandwiches = []
+sandwich_orders = ["tuna", "egg", "chicken", "vegan"] #list of sandwiches
+finished_sandwiches = [] #empty list to store popped sandwiches
 
-while sandwich_orders:
+while sandwich_orders: #loops through the sandwich orders
     sandwich_popped = sandwich_orders.pop() #pops each sandwich
-    print(f"Your {sandwich_popped} sandwich is currently in the works!")
-    finished_sandwiches.append(sandwich_popped) #adds the popped sandwiches in the finished sandwich list
+    print(f"Your {sandwich_popped} sandwich is currently in the works!") #informs user that the sandwiches are in the works
+    finished_sandwiches.append(sandwich_popped) #adds the popped sandwiches in the empty list
 
-print("\n")
+print("\n") #adds whitespace
 
-for sandwich_popped in finished_sandwiches: #prints the ready sandwiches
-    print(sandwich_popped.title(),"sandwich is ready!")   
+for sandwich_popped in finished_sandwiches: #displays the finished sandwiches
+    print(sandwich_popped.title(),"sandwich is ready!") #informs user that the sandwiches are ready

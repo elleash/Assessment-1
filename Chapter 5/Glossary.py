50dd451d171ba9eb049a1bbe616d7696d86886d4
elleash <105912749+elleash@users.noreply.github.com>
2022-12-16 14:38:27 +0800

    Update Glossary.py

--- Chapter 5/Glossary.py
+++ Chapter 5/Glossary.py
@@ -1,22 +1,20 @@
-#dictionary of python terms
 glossary = {"variable": "A variable is a name referring to an object or value.", 
     "string": "A string is series of words or characters.",
     "loop" : "Loops are utilised when we need to run statements repeatedly until a specific condition has been executed.",
     "list" : "A set of values that are mutable and flexible.",
-    "dictionary" : "A dictionary is and ordered collection of key-value pairs."}
+    "dictionary" : "A dictionary is and ordered collection of key-value pairs."} #dictionary of python terms
 
-#series of print statements to print each word and its meaning properly 
-print("variable :".title())
-print(glossary["variable"], "\n")
+print("variable :".title()) #diplays the item "variable"
+print(glossary["variable"], "\n") #displays the meaning of the item "variable"
 
-print("string :".title())
-print(glossary["string"], "\n")
+print("string :".title()) #diplays the item "string"
+print(glossary["string"], "\n") #displays the meaning of the item "string"
 
-print("loop :".title())
-print(glossary["loop"], "\n")
+print("loop :".title()) #diplays the item "loop"
+print(glossary["loop"], "\n") #displays the meaning of the item "loop"
 
-print("list :".title())
-print(glossary["list"], "\n")
+print("list :".title()) #diplays the item "list"
+print(glossary["list"], "\n") #displays the meaning of the item "list"
 
-print("dictionary :".title())
-print(glossary["dictionary"], "\n")
+print("dictionary :".title()) #diplays the item "dictionary"
+print(glossary["dictionary"], "\n") #displays the meaning of the item "dictionary"

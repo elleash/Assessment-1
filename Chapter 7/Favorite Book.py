e867073037f16c67608406a6627e5492aa23ab91
elleash <105912749+elleash@users.noreply.github.com>
2022-12-16 15:05:38 +0800

    Update Favorite Book.py

--- Chapter 7/Favorite Book.py
+++ Chapter 7/Favorite Book.py
@@ -1,4 +1,4 @@
-def favorite_book(title): #function with on parameter
+def favorite_book(title): #function with one parameter
     print(f"My favorite book is {title.title()}!") #message to be printed
 
 favorite_book("the seven husbands of evelynn hugo") #calls book title with an argument

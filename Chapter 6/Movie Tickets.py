7c7425962b47592e6a3f49848dea7c26c4370e44
elleash <105912749+elleash@users.noreply.github.com>
2022-12-16 14:54:15 +0800

    Update Movie Tickets.py

--- Chapter 6/Movie Tickets.py
+++ Chapter 6/Movie Tickets.py
@@ -1,12 +1,11 @@
 while True: #loop for asking the age continously
     age = int(input("Enter the age to find out the ticket price or enter 'quit' to exit.:")) #asks user to enter age
 
-#if-elif-else statement for the ticket price according to the ages
-    if age == "quit":
-                break
-    if age < 3:
-        print("Your ticket is free!")
-    elif age < 12:
-        print("Your ticket is $10!")
-    else:
-        print("Your ticket is $15")
+    if age == "quit": #if user enters "quit", it will stop the code
+                break #ends the code
+    if age < 3: #if user is below 3, then the ticket is free
+        print("Your ticket is free!") #displays the ticket price according to age
+    elif age < 12:  #if user is below 12, then the ticket is $10
+        print("Your ticket is $10!") #displays the ticket price according to age
+    else:  #if user is above 12, then the ticket is $15
+        print("Your ticket is $15") #displays the ticket price according to age

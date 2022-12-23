46d0f9071bc4eab218444ba51e8d550a92b9b5df
elleash <105912749+elleash@users.noreply.github.com>
2022-12-23 17:46:52 +0800

    Update Strings Concatination.py

--- Chapter 1/Strings Concatination.py
+++ Chapter 1/Strings Concatination.py
@@ -2,4 +2,4 @@ x = input("Enter your first subject:") #asks the user to input their subjects
 y = input("Enter your second subject:") #asks the user to input their subjects
 z = input("Enter your third subject:") #asks the user to input their subjects
 
-print("Your schedule is:", x+" "+y+" "+z) #using the + operator, the subject would be printed together
+print("Your schedule is:", x+", "+y+", "+z) #using the + operator, the subject would be printed together
